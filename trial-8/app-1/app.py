c0e619d9ba14507f8671c0feeb22ff8a7c563c5a
root <root@monitoring.asia-southeast2-a.c.diesel-horizon-384008.internal>
2023-04-27 10:50:45 +0000

    update python

---
+++ trial-8/app-1/app.py
@@ -0,0 +1,27 @@
+import json
+import flask
+from time import sleep
+import logging
+import socket
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+logging.basicConfig(level=logging.INFO)
+
+@app.route('/hello')
+def hello():
+    hostname = socket.gethostname()
+    return f'Hello World! from APP 1 and node {hostname}'
+
+@app.route('/get', methods=['GET'])
+def getMethod():
+    jsonReq = flask.request.json
+    jsonResp = {}
+    try:
+        logging.info(jsonReq)
+    except IOError as identifier:
+        print(identifier)
+    return jsonResp
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
